41c2aba6e93814213f90bfb66abad3d0dd0c24ea
SantiagoSandoval <sandovalbgasantiago@gmail.com>
2024-03-07 15:20:51 -0500

    sano

--- MySQL/lesson_6.sql
+++ MySQL/lesson_6.sql
@@ -166,7 +166,7 @@ SELECT CT.name AS Ciudades_Continentes, C.continent AS Continente, CT.Population
 FROM city AS CT
 RIGHT JOIN country AS C ON CT.countrycode = C.code
 WHERE C.continent <> "Oceania"
-ORDER BY Poblacion DESC, Continente, Ciudades_Continentes DESC LIMIT 5 ;
+ORDER BY Poblacion DESC, Continente, Ciudades_Continentes DESC LIMIT 5;
 
 /*
 3. Idiomas oficiales:
@@ -201,7 +201,127 @@ ORDER BY Lenguajes_Santander;
 /*
 5. Países sin lenguaje oficial:
 • Encontrar los países que no tienen idioma oficial registrado.
+*/
+
+    SELECT C.name
+    FROM country AS C
+    RIGHT JOIN countrylanguage AS CL ON C.code = CL.countrycode
+    WHERE CL.isofficial <> "T";
+
+
+/*
 7. Ciudades habla hispanas de Asia:
 • Mostrar la ciudades de Asia y su país, ordenadas por la cantidad de habitantes, que
 tienen como idioma español.
+*/
+    SELECT CT.name, CT.population
+    FROM city AS CT
+    RIGHT JOIN country AS C ON CT.countrycode = C.code
+    WHERE C.continent = "Asia";
+
+/* PUNTO II
+    SCRIPT CREACION TIENDA: */
+DROP DATABASE IF EXISTS tienda;
+CREATE DATABASE tienda CHARACTER SET utf8mb4;
+USE tienda;
+CREATE TABLE fabricante (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL
+);
+CREATE TABLE producto (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+precio DOUBLE NOT NULL,
+id_fabricante INT UNSIGNED NOT NULL,
+FOREIGN KEY (id_fabricante) REFERENCES fabricante(id)
+);
+INSERT INTO fabricante VALUES(1, 'Asus');
+INSERT INTO fabricante VALUES(2, 'Lenovo');
+INSERT INTO fabricante VALUES(3, 'Hewlett-Packard');
+INSERT INTO fabricante VALUES(4, 'Samsung');
+INSERT INTO fabricante VALUES(5, 'Seagate');
+INSERT INTO fabricante VALUES(6, 'Crucial');
+INSERT INTO fabricante VALUES(7, 'Gigabyte');
+INSERT INTO fabricante VALUES(8, 'Huawei');
+INSERT INTO fabricante VALUES(9, 'Xiaomi');
+INSERT INTO producto VALUES(1, 'Disco duro SATA3 1TB', 86.99, 5);
+INSERT INTO producto VALUES(2, 'Memoria RAM DDR4 8GB', 120, 6);
+INSERT INTO producto VALUES(3, 'Disco SSD 1 TB', 150.99, 4);
+INSERT INTO producto VALUES(4, 'GeForce GTX 1050Ti', 185, 7);
+INSERT INTO producto VALUES(5, 'GeForce GTX 1080 Xtreme', 755, 6);
+INSERT INTO producto VALUES(6, 'Monitor 24 LED Full HD', 202, 1);
+INSERT INTO producto VALUES(7, 'Monitor 27 LED Full HD', 245.99, 1);
+INSERT INTO producto VALUES(8, 'Portátil Yoga 520', 559, 2);
+INSERT INTO producto VALUES(9, 'Portátil Ideapd 320', 444, 2);
+INSERT INTO producto VALUES(10, 'Impresora HP Deskjet 3720', 59.99, 3);
+INSERT INTO producto VALUES(11, 'Impresora HP Laserjet Pro M26nw', 180, 3);
+
 /*
+
+Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
+productos de la base de datos.
+*/
+
+SELECT P.nombre, P.precio, F.nombre
+FROM producto AS P, fabricante AS F
+RIGHT JOIN fabricante AS F ON P.id_fabricante = F.id; 
+
+-- ??????? 
+
+/*
+2. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los
+productos de la base de datos. Ordene el resultado por el nombre del fabricante, por orden
+alfabético.*/
+
+SELECT P.nombre, P.precio, F.nombre
+FROM producto AS P, fabricante AS F
+RIGHT JOIN fabricante AS F ON P.id_fabricante = F.id
+ORDER BY F.nombre; 
+
+/*
+3. Devuelve una lista con el identificador del producto, nombre del producto, identificador del
+fabricante y nombre del fabricante, de todos los productos de la base de datos.*/
+
+SELECT P.*, F.nombre
+FROM producto AS P
+RIGHT JOIN fabricante AS F ON P.id_fabricante = F.
+
+/*
+4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
+barato.
+5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más
+caro.
+6. Devuelve una lista de todos los productos del fabricante Lenovo.
+7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor
+que 200€.
+8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
+Seagate. Sin utilizar el operador IN.
+9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy
+Seagate. Utilizando el operador IN.
+10. Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo
+nombre termine por la vocal e.
+11. Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de
+fabricante contenga el carácter w en su nombre.
+12. Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los
+productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar
+por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+13. Devuelve un listado con el identificador y el nombre de fabricante, solamente de aquellos
+fabricantes que tienen productos asociados en la base de datos.
+14. Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los
+productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes
+que no tienen productos asociados.
+15. Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún
+producto asociado.
+16. ¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su
+respuesta.
+17. Devuelve todos los productos del fabricante Lenovo. (Sin utilizar INNER JOIN).
+
+18. Devuelve todos los datos de los productos que tienen el mismo precio que el producto más
+caro del fabricante Lenovo. (Sin utilizar INNER JOIN).
+19. Lista el nombre del producto más caro del fabricante Lenovo.
+20. Lista el nombre del producto más barato del fabricante Hewlett-Packard.
+21. Devuelve todos los productos de la base de datos que tienen un precio mayor o igual al
+producto más caro del fabricante Lenovo.
+22. Lista todos los productos del fabricante Asus que tienen un precio superior al precio medio
+de todos sus productos.
+*/
